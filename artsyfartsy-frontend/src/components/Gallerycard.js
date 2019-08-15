db7d7153b3c09181f81ff8a58bfb31dcb45da332
Spencer Lindemuth <lindemut@gmail.com>
2019-08-15 15:23:00 -0700

    pictures position absolute

--- artsyfartsy-frontend/src/components/Gallerycard.js
+++ artsyfartsy-frontend/src/components/Gallerycard.js
@@ -13,17 +13,29 @@ class Gallerycard extends React.Component {
   }
 
   saveCoords = (ev) => {
-
+    let element = ev.target
+    let rect = element.getBoundingClientRect();
+    this.setState=({
+        xCord: rect.left,
+        yCord: rect.top
+    })
+    localStorage.setItem(`xCord${this.props.card.id}`, rect.left)
+    localStorage.setItem(`yCord${this.props.card.id}`, rect.top)
   }
 
+  componentWillMount = () => {
+      if(localStorage.getItem(`xCord${this.props.card.id}`) && localStorage.getItem(`yCord${this.props.card.id}`)){
+       this.setState({xCord: localStorage.getItem('xCord'), yCord: localStorage.getItem('yCord')})
+      }else{
+          this.setState({xCord: 100, yCord: 100})
+      }
+  }
 
   render() {
     return (
-      <DraggableCore grid={[1,1]} onDrag={this.setCoords} onStop={this.saveCoords}>
-          <span className="gallerycard" onClick={() => this.props.handleClick(this.props.card)} className='explorecard' id={this.props.card.id}>
+      <Draggable grid={[1,1]} onStop={this.saveCoords} defaultPosition={{x: this.state.xCord, y: this.state.yCord}} bounds={"parent"}>
           <img src={this.props.card.primaryImage} alt='' draggable="false"/>
-          </span>
-        </DraggableCore>
+        </Draggable>
     );
   }
 
