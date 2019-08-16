f27cf3209604246cf8a7a3124b41f22010c86ce6
Claire Wilbert <claire.wilbert@gmail.com>
2019-08-16 09:45:52 -0700

    home page start

---
+++ artsyfartsy-frontend/src/components/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Homepage extends React.Component {
+  state = {}
+
+  render() {
+    return(
+      <div>
+        Welcome to Artsy-Fartsy!
+      </div>
+    )
+  }
+}
