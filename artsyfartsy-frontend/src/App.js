d25c6e5c3c4065a573cfeaa1aa0791bcd64cb8d3
Spencer Lindemuth <lindemut@gmail.com>
2019-08-19 09:33:44 -0700

    set bounds and stuff

--- artsyfartsy-frontend/src/App.js
+++ artsyfartsy-frontend/src/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar'
 import Gallery from './components/Gallery'
-import Expand from './components/Expand'
 import {BrowserRouter as Router, Route, Link, NavLink} from 'react-router-dom'
 
 
 class App extends React.Component {
   state = {
     imageSrc: '',
-    expand: [],
-    top: []
+    explore: []
   }
 
 
@@ -23,6 +21,9 @@ class App extends React.Component {
         </Router>
       </div>
     )
+  }
+  componentDidMount() {
+
   }
 }
 
--- artsyfartsy-frontend/src/components/Gallery.js
+++ artsyfartsy-frontend/src/components/Gallery.js
@@ -9,6 +9,16 @@ class Gallery extends React.Component {
     expand: [],
     top: []
   }
+
+  getGallery(){
+    fetch(`http://localhost:3000/users/${3}/gallery`)
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        top: [...data]
+      })
+    })
+  }
   
   loadImage = () => {
     fetch('http://localhost:3000/pieces').then(res => res.json()).then(data => {
@@ -25,24 +35,30 @@ class Gallery extends React.Component {
     })
   }
 
-  componentDidMount() {
-    this.loadImage()
-  }
-
   addToGallery = (card) => {
-
-    if (this.state.top.length <6) {
-      let pieces = this.state.expand.filter((piece) => {
-        return piece !== card
-      })
-      this.setState({
-        top: [...this.state.top, card],
-        expand: [...pieces]
-      })
-    } else {
-      console.log('You have enough Old Masters.')
-    }
-
+    //filter this bitch
+    if(this.state.top.includes(card)){
+      console.log('This piece already in your gallery')
+    }else{
+      if (this.state.top.length <6) {
+        let pieces = this.state.expand.filter((piece) => {
+          return piece !== card
+        })
+        fetch("http://localhost:3000/users/add", {
+          method: "POST",
+          headers: {"content-type": "application/json"},
+          body: JSON.stringify({user_id: 3, piece: card})
+        })
+        .then(res => {
+          this.setState({
+            top: [...this.state.top, card],
+            expand: [...pieces]
+          })
+        })
+      } else {
+        console.log('You have enough Old Masters.')
+      }
+    } 
   }
 
   removeFromGallery = (card) => {
@@ -62,7 +78,7 @@ class Gallery extends React.Component {
         <div id="galleryBackground">
           <div className="galleryWall">
             {this.state.top.map(pic => {
-              return <Gallerycard card={pic} key={pic.id} handleClick={() => null} />
+              return <Gallerycard card={pic} key={pic.id} userId={3} handleClick={() => null} />
             })}
           </div>
         </div>
@@ -70,6 +86,11 @@ class Gallery extends React.Component {
         <Expand handleClick={this.addToGallery} expand={this.state.expand} />
       </div>
     )
+  }
+
+  componentDidMount() {
+    this.getGallery()
+    this.loadImage()
   }
 
 }
