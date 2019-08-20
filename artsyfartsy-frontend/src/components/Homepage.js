c37838b8e32b7754f631e267dc8d1bdbc56ac2fa
Spencer Lindemuth <lindemut@gmail.com>
2019-08-20 15:27:26 -0700

    Login Form

--- artsyfartsy-frontend/src/components/Homepage.js
+++ artsyfartsy-frontend/src/components/Homepage.js
@@ -3,13 +3,56 @@ import Logo from '../images/logo.png'
 import { BrowserRouter as NavLink } from 'react-router-dom' 
 
 class Homepage extends React.Component {
-  state = {}
+  state = {
+    username: "",
+    password: ""
+  }
 
+  handleUsernameChange = (ev) => {
+    this.setState({
+      username: ev.target.value
+    })
+  }
+
+  handlePasswordChange = (ev) => {
+    this.setState({
+      password: ev.target.value
+    })
+  }
+
+
+  handleSumbit = (ev) => {
+    ev.preventDefault()
+    this.setState({
+      username: "",
+      password: ""
+    })
+    fetch("http://localhost:3000/tokens", {
+      method: "POST",
+      headers: {"content-type" : "application/json"},
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    }).then(res => res.json())
+    .then(data => {
+      if(data.status){
+        console.log("error", data)
+      }else{
+        console.log("logged in", data)
+      }
+    })
+  }
 
 
   render() {
     return(
       <div>
+        <form onSubmit={this.handleSumbit}>
+          <input type="text" value={this.state.username} placeholder="Username" onChange={this.handleUsernameChange} />
+          <input type="password" value={this.state.password} placeholder="Password" onChange={this.handlePasswordChange}/>
+          <input type="submit" value="Log In" />
+        </form>
         <img id="logo" src={Logo} alt='brilliant logo' />
       </div>
     )
