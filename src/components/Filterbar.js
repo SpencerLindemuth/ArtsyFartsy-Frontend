ea3373b282331c9c8eea356a30e75571eb6199d9
Claire Wilbert <claire.wilbert@gmail.com>
2019-08-21 15:58:07 -0700

    and done

--- src/components/Filterbar.js
+++ src/components/Filterbar.js
@@ -30,19 +30,19 @@ class Filterbar extends React.Component {
           <select value={this.props.departmentIs} onChange={this.props.showDepartment} >
             <option value="Select a department">View All</option>
             <option value="European Paintings">European Paintings</option>
-            <option value="The Robert Lehman Collection">The Robert Lehman Collection</option>
             <option value="Greek and Roman Art">Greek & Roman Art</option>
+            <option value="Robert Lehman Collection">The Robert Lehman Collection</option>
           </select>
         </div>
         <div>
           <label>Filter by Artist Nationality</label>
           <select value={this.props.artistNatIs} onChange={this.props.showArtistNat}>
             <option value="Select a nationality">View All</option>
-            <option value="French">French</option>
-            <option value="Italian">Italian</option>
             <option value="British">British</option>            
             <option value="Dutch">Dutch</option>            
+            <option value="French">French</option>
             <option value="German">German</option>            
+            <option value="Italian">Italian</option>
             <option value="Russian">Russian</option>
             <option value="">Nationality not given</option>
           </select>
